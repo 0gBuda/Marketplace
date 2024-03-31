55fb28643701f677b4275c77e4a53c8c3a688210
Maksik <empty>
2024-03-31 18:02:23 +0300

    Second

--- main/views.py
+++ main/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+from goods.models import Categories
+
 def index(request):
-    return render(request,'main/index.html')
+
+    categories = Categories.objects.all()
+
+
+    context = {
+        'content': "Магазин мебели HOME",
+        'categories': categories
+    }
+
+    return render(request, 'main/index.html', context)
+
+
