a7e6f0230f18dc793243195fa4fd2e8e84f259bf
Maksik <empty>
2024-04-12 16:35:11 +0300

    PostgresSQL database connected and full text search

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     "debug_toolbar",
 
@@ -83,8 +84,13 @@ WSGI_APPLICATION = 'Marketplace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'marketplace',
+        'USER': 'maksik',
+        'PASSWORD': 'Yfdfkmysq2020',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
---
+++ goods/utils.py
@@ -0,0 +1,26 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.db.models import Q
+
+from goods.models import Products
+
+
+def q_search(query):
+    if query.isdigit() and len(query) <= 5:
+        return Products.objects.filter(id=int(query))
+
+    vector = SearchVector("name", "descriptions")
+    query = SearchQuery(query)
+
+    return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+
+
+    # keywords = [word for word in query.split() if len(word) > 2]
+    #
+    #
+    # q_object = Q()
+    #
+    # for token in keywords:
+    #     q_object |= Q(descriptions__icontains=token)
+    #     q_object |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_object)
